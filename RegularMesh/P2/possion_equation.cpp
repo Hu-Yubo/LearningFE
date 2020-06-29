1a851bfa469ad8b906bcc96bdf01ea4b9a9b32da
Hu-Yubo <21935029@zju.edu.cn>
2020-06-29 11:40:01 +0800

    Keep going

--- RegularMesh/P2/possion_equation.cpp
+++ RegularMesh/P2/possion_equation.cpp
@@ -1,9 +1,9 @@
 /**
  * @file   possion_equation.cpp
  * @author StudyBo <studybo@ubuntu18-04>
- * @date   Fri Jun 26 17:11:00 2020
+ * @date   Sat Jun 27 16:08:07 2020
  * 
- * @brief  Solve 2D possion equation, P1 case.
+ * @brief  Solve 2D possion equation, P2 case.
  * 
  * 
  */
@@ -64,7 +64,7 @@ double f(const double* p)
  */
 
 /// 从 j 行 i 列，每一行 2n 个网格的 P1 剖分中映射 (i,j) 单元的第 k 个自由度编号。
-int P1_ele2dof(int n, int j, int i, int k)
+int P2_ele2dof(int n, int j, int i, int k)
 {
     int dof = -1;
     if ( i % 2 == 0)
@@ -72,13 +72,22 @@ int P1_ele2dof(int n, int j, int i, int k)
 	switch(k)
 	{
 	case 0:
-	    dof = j * (n + 1) + i / 2;
+	    dof = 2 * j * (2 * n + 1) + i;
+	    break;
+	case 4:
+	    dof = (2 * j + 1) * (2 * n + 1) + i + 1;
 	    break;
 	case 1:
-	    dof = (j + 1) * (n + 1) + i / 2 + 1;
+	    dof = 2 * (j + 1) * (2 * n + 1) + i + 2;
+	    break;
+	case 3:
+	    dof = 2 * (j + 1) * (2 * n + 1) + i + 1;
 	    break;
 	case 2:
-	    dof = (j + 1) * (n + 1) + i / 2;
+	    dof = 2 * (j + 1) * (2 * n + 1) + i;
+	    break;
+	case 5:
+	    dof = (2 * j + 1) * (2 * n + 1) + i;
 	    break;
 	default:
 	    std::cerr << "Dof. no. error!" << std::endl;
@@ -90,13 +99,22 @@ int P1_ele2dof(int n, int j, int i, int k)
 	switch(k)
 	{
 	case 0:
-	    dof = j * (n + 1) + i / 2;
+	    dof = 2 * j * (2 * n + 1) + i - 1;
+	    break;
+	case 4:
+	    dof = 2 * j * (2 * n + 1) + i;
 	    break;
 	case 1:
-	    dof = j * (n + 1) + i / 2 + 1;
+	    dof = 2 * j * (2 * n + 1) + i + 1;
+	    break;
+	case 3:
+	    dof = (2 * j + 1) * (2 * n + 1) + i + 1;
 	    break;
 	case 2:
-	    dof = (j + 1) * (n + 1) + i / 2 + 1;
+	    dof = 2 * (j + 1) * (2 * n + 1) + i + 1;
+	    break;
+	case 5:
+	    dof = (2 * j + 1) * (2 * n + 1) + i;
 	    break;
 	default:
 	    std::cerr << "Dof. no. error!" << std::endl;
@@ -106,84 +124,44 @@ int P1_ele2dof(int n, int j, int i, int k)
     return dof;
 }
 
-/// 从 j 行 i 列，每一行 2n 个网格的 P1 剖分中映射 (i,j) 单元的第 k 个顶点坐标。
-AFEPack::Point<2> P1_ele2vtx(int n, int j, int i, int k)
+/// 每个编号为 dof 的自由度所对应的节点
+AFEPack::Point<2> Dof_to_vtx(int n, int dof)
 {
     AFEPack::Point<2> pnt;
-    if (i % 2 == 0)
-    {
-	switch (k)
-	{
-	case 0:
-	    pnt[0] = ((n - i / 2) * xa + i / 2 * xb) / n;
-	    pnt[1] = ((n - j) * ya + j * yb) / n;
-	    break;
-	case 1:
-	    pnt[0] = ((n - i / 2 - 1) * xa + (i / 2 + 1) * xb) / n;
-	    pnt[1] = ((n - j - 1) * ya + (j + 1) * yb) / n;
-	    break;
-	case 2:
-	    pnt[0] = ((n - i / 2) * xa + i / 2 * xb) / n;
-	    pnt[1] = ((n - j - 1) * ya + (j + 1) * yb) / n;
-	    break;
-	default:
-	    std::cerr << "Element vertex no. error!" << std::endl;
-	    exit(-1);
-	}
-    }
-    else
-    {
-	switch (k)
-	{
-	case 0:
-	    pnt[0] = ((n - i / 2) * xa + i / 2 * xb) / n;
-	    pnt[1] = ((n - j) * ya + j * yb) / n;
-	    break;
-	case 1:
-	    pnt[0] = ((n - i / 2 - 1) * xa + (i / 2 + 1) * xb) / n;
-	    pnt[1] = ((n - j) * ya + j * yb) / n;
-	    break;
-	case 2:
-	    pnt[0] = ((n - i / 2 - 1) * xa + (i / 2 + 1) * xb) / n;
-	    pnt[1] = ((n - j - 1) * ya + (j + 1) * yb) / n;
-	    break;
-	default:
-	    std::cerr << "Element vertex no. error!" << std::endl;
-	    exit(-1);
-	}
-    }
+    /// j 行 i 列
+    int j = dof / (2 * n + 1);
+    int i = dof % (2 * n + 1);
+    pnt[0] = i * (xb - xa) / (n * 2);
+    pnt[1] = j * (yb - ya) / (n * 2);
+    return pnt;
+}
+
+/// 从 j 行 i 列，每一行 2n 个网格的 P1 剖分中映射 (i,j) 单元的第 k 个顶点坐标。
+AFEPack::Point<2> P2_ele2vtx(int n, int j, int i, int k)
+{
+    AFEPack::Point<2> pnt;
+    int dof = P2_ele2dof(n, j, i, k);
+    pnt = Dof_to_vtx(n, dof);
     return pnt;
 }
 
 /// 储存所有边界自由度编号。
 int Store_BndDof(int n)
 {
-    for (int i = 0; i < n + 1; i++)
+    for (int i = 0; i < 2 * n + 1; i++)
     {
 	BndDof.push_back(i);
-	BndDof.push_back((n + 1) * (n + 1) - 1 - i);
+	BndDof.push_back((2 * n + 1) * (2 * n + 1) - 1 - i);
     }
-    if (n > 1)
+    if (n >= 1)
     {
-	for (int i = 1; i < n; i++ )
+	for (int i = 1; i < 2 * n; i++)
 	{
-	    BndDof.push_back(i * (n + 1));
-	    BndDof.push_back(i * (n + 1) + n);
+	    BndDof.push_back(i * (2 * n + 1));
+	    BndDof.push_back(i * (2 * n + 1) + 2 * n);
 	}
     }
     return 0;
-}
-
-/// 每个编号为 dof 的自由度所对应的节点
-AFEPack::Point<2> Dof_to_vtx(int n, int dof)
-{
-    AFEPack::Point<2> pnt;
-    /// j 行 i 列
-    int j = dof / (n + 1);
-    int i = dof % (n + 1);
-    pnt[0] = i * (xb - xa) / n;
-    pnt[1] = j * (yb - ya) / n;
-    return pnt;
 }
 
 int main(int argc, char* argv[])
@@ -194,10 +172,10 @@ int main(int argc, char* argv[])
     CoordTransform<2, 2> triangle_coord_transform;
     triangle_coord_transform.readData("triangle.crd_trs");
     TemplateDOF<2> triangle_template_dof(triangle_template_geometry);
-    /// 一次元
-    triangle_template_dof.readData("triangle.1.tmp_dof");
+    /// 二次元
+    triangle_template_dof.readData("triangle.2.tmp_dof");
     BasisFunctionAdmin<double, 2, 2> triangle_basis_function(triangle_template_dof);
-    triangle_basis_function.readData("triangle.1.bas_fun");
+    triangle_basis_function.readData("triangle.2.bas_fun");
     TemplateElement<double, 2, 2> template_element;
     /// 模板单元初始化
     template_element.reinit(triangle_template_geometry,
@@ -214,37 +192,59 @@ int main(int argc, char* argv[])
     int n_dof = template_element.n_dof();
     /// 基函数个数
     int n_bas = template_element.basisFunction().size();
-    
-    /// 产生一个具体单元顶点的缓存。一个三角形的 3 个顶点。这里其实是这
-    /// 三个顶点正好是 P1 单元的 3 个单元内自由度。gv 表示全局的三角形坐
-    /// 标，就是在物理计算区域内一个网格的顶点坐标；而 lv 表示局部的三角
-    /// 形坐标，即参考单元的坐标。沿用了 AFEPack 的配置，是固定的 [0,
-    /// 0]-[0, 1]-[1, 0]。
 
-    /// 全局点
-    std::vector<AFEPack::Point<2> > gv(3);
     /// 观察一下模板单元中的自由度，基函数和基函数在具体积分点的取值情况，可从模板单元中读取到。
-    TemplateGeometry<2> &geo = template_element.geometry();
-    const std::vector<AFEPack::Point<2> > &lv = geo.vertexArray();
-    int n_vtx = geo.n_geometry(0);
+    int n_vtx = 6;
+    /// 局部点，参考单元的 6 个节点， 取点参照 triangle.2.bas_fun。
+    std::vector<AFEPack::Point<2> > lv(n_vtx);
+    lv[0][0] = 0.0;  lv[0][1] = 0.0;
+    lv[1][0] = 1.0;  lv[1][1] = 0.0;
+    lv[2][0] = 0.0;  lv[2][1] = 1.0;
+    lv[3][0] = 0.5;  lv[3][1] = 0.5;
+    lv[4][0] = 0.0;  lv[4][1] = 0.5;
+    lv[5][0] = 0.5;  lv[5][1] = 0.0;
+    /// 全局点
+    std::vector<AFEPack::Point<2> > gv(n_vtx);
     /// 设置剖分段数。
-    int n = 50;
+    int n = 20;
     /// 自由度总数
-    int dim = (n + 1) * (n + 1);
+    int dim = (2 * n + 1) * (2 * n + 1);
     /// 右端项
     Vector<double> rhs(dim);
     /// 记得先储存边界自由度！！！！！！！！
     Store_BndDof(n);
-    /// 稀疏矩阵中每行对应的非零元个数，每行最多 7 个非零元。
-    std::vector<unsigned int> NoZeroPerRow(dim, 7);
-    /// 非角点的边界自由度所在行有 5 个非零元。
-    for (int i = 0; i < BndDof.size(); i++)
-	NoZeroPerRow[BndDof[i]] = 5;
-    /// 角点自由度所在行有 3 或 4 个非零元。
-    NoZeroPerRow[0] = 4;
-    NoZeroPerRow[dim - 1] = 4;
-    NoZeroPerRow[n] = 3;
-    NoZeroPerRow[dim - n - 1] = 3;
+    /// 稀疏矩阵中每行对应的非零元个数，大部分非零元个数为 9。
+    std::vector<unsigned int> NoZeroPerRow(dim, 9);
+    /// 利用对称性遍历一半的非零元个数。
+    for (int dof = 0; dof <= dim / 2; dof++)
+    {
+	int j = dof / (2 * n + 1);
+	int i = dof % (2 * n + 1);
+	if (j % 2 == 0)
+	{
+	    if ( i % 2 == 0)
+		NoZeroPerRow[dof] = 19;
+	    if (i == 0 || i == 2 * n)
+		NoZeroPerRow[dof] = 12;
+	}
+	else
+	{
+	    if (i == 0 || i == 2 * n)
+		NoZeroPerRow[dof] = 6;
+	}   
+	if (j == 0)
+	{
+	    if (i % 2 == 0)
+		NoZeroPerRow[dof] = 12;
+	    else
+		NoZeroPerRow[dof] = 6;
+	    if (i == 0)
+		NoZeroPerRow[dof] = 9;
+	    if (i == 2 * n)
+		NoZeroPerRow[dof] = 6;
+	}
+	NoZeroPerRow[dim - 1 - dof] = NoZeroPerRow[dof];
+    }
     /// 建立稀疏矩阵模板。
     SparsityPattern sp_stiff_matrix(dim, NoZeroPerRow);
     /// 填充非零元素对应的行索引和列索引，遍历顺序按照单元的顺序。
@@ -255,8 +255,8 @@ int main(int argc, char* argv[])
 	    for (int dof1 = 0; dof1 < n_dof; dof1++)
 		for (int dof2 = 0; dof2 < n_dof; dof2++)
 		{
-		    sp_stiff_matrix.add(P1_ele2dof(n, j, i, dof1),
-			                P1_ele2dof(n, j, i, dof2));
+		    sp_stiff_matrix.add(P2_ele2dof(n, j, i, dof1),
+			                P2_ele2dof(n, j, i, dof2));
 		}
 	}
     }
@@ -265,13 +265,12 @@ int main(int argc, char* argv[])
     /// 刚度矩阵初始化。
     SparseMatrix<double> stiff_mat(sp_stiff_matrix);
     /// 生成节点，单元，刚度矩阵和右端项。
-    // double h = (xb - xa) / n;
     for (int j = 0; j < n; j++)
 	for (int i = 0; i < 2 * n; i++)
 	{
 	    /// 实际单元顶点坐标。。
 	    for (int k = 0; k < n_vtx; k++)
-		gv[k] = P1_ele2vtx(n ,j, i, k);
+		gv[k] = P2_ele2vtx(n, j, i, k);
 	    /// 积分
 	    for (int l = 0; l < n_quadrature_point; l++)
 	    {
@@ -283,15 +282,15 @@ int main(int argc, char* argv[])
 		for (int base1 = 0; base1 < n_dof; base1++)
 		{
 		    for (int base2 = 0; base2 < n_dof; base2++)
-			stiff_mat.add(P1_ele2dof(n, j, i, base1),
-			              P1_ele2dof(n, j, i, base2),
+			stiff_mat.add(P2_ele2dof(n, j, i, base1),
+			              P2_ele2dof(n, j, i, base2),
 			              Jxy * innerProduct(triangle_basis_function[base1].gradient(point[l], gv), triangle_basis_function[base2].gradient(point[l], gv)));
 		    /// 右端项
-		    rhs(P1_ele2dof(n, j, i, base1)) += Jxy * f(point[l]) * triangle_basis_function[base1].value(point[l], gv);
+		    rhs(P2_ele2dof(n, j, i, base1)) += Jxy * f(point[l]) * triangle_basis_function[base1].value(point[l], gv);
 		}
 	    }
 	}
-    /// 处理边界条件
+     /// 处理边界条件
     for (int i = 0; i < BndDof.size(); i++)
     {
 	int index = BndDof[i];
@@ -326,18 +325,18 @@ int main(int argc, char* argv[])
     Vector<double> solution(dim);
     double tol = std::numeric_limits<double>::epsilon() * dim;
     solver.solve(solution, rhs, tol, 10000);
-    std::ofstream fs;
     /// 输出到 output.m
+    std::ofstream fs;
     fs.open("output.m");
-    fs << "x = " << xa << ":" << xb - xa << "/" << n << ":" << xb << ";" << std::endl;
-    fs << "y = " << ya << ":" << yb - ya << "/" << n << ":" << yb << ";" << std::endl;
+    fs << "x = " << xa << ":" << xb - xa << "/" << 2*n << ":" << xb << ";" << std::endl;
+    fs << "y = " << ya << ":" << yb - ya << "/" << 2*n << ":" << yb << ";" << std::endl;
     fs << "[X, Y] = meshgrid(x, y);" << std::endl;
     fs << "u = [";
-    for (int j = 0; j < n + 1; j++)
+    for (int j = 0; j < 2 * n + 1; j++)
     {
-	for (int i = 0; i < n + 1; i++)
+	for (int i = 0; i < 2 * n + 1; i++)
 	{
-	    fs << solution[i + (n + 1) * j] << " , ";
+	    fs << solution[i + (2 * n + 1) * j] << " , ";
 	}
 	fs << ";" << std::endl;
     }
@@ -355,10 +354,6 @@ int main(int argc, char* argv[])
     }
     error = std::sqrt(error);
     std::cerr << "\nL2 error = " << error << ", tol = " << tol << std::endl;
-
     return 0;
 }
-    
 
-    
-	    
